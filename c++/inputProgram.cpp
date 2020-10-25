3e8426c2b730463ecc39e8eae3975f09f8b55650
pm1975 <pm1975@windowslive.com>
2020-10-25 21:31:41 +0100

    first commit

---
+++ c++/inputProgram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string firstName, lastName;
+  cout << "Enter your first name: ";
+  cin >> firstName;
+
+  cout << "Enter your last name: ";
+  cin >> lastName;
+
+  cout << "Your full name is: " << firstName << " " << lastName << endl;
+
+  return 0;
+}
